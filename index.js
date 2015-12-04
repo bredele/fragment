3f96e21b6bbfe61eed0b659f1b21125b8e0942c8
bredele <olivier.wietrich@gmail.com>
2015-12-03 21:52:00 -0700

    expose anonymous function

--- index.js
+++ index.js
@@ -7,7 +7,7 @@
  * @api public
  */
 
-module.exports = function fragment(arr) {
+module.exports = function(arr) {
   var el = document.createDocumentFragment();
   for(var i = 0, l = arr.length; i < l; i++) {
     el.appendChild(arr[i]));
