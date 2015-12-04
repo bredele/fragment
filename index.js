e67417b0beea75e59763751bd4beb6f6b521bf99
bredele <olivier.wietrich@gmail.com>
2015-12-03 21:45:48 -0700

    add fragment handler

---
+++ index.js
@@ -0,0 +1,16 @@
+
+/**
+ * Fragment array of nodes.
+ *
+ * @param {Array} arr
+ * @return {DocumentFragment}
+ * @api public
+ */
+
+module.exports = function fragment(arr) {
+  var el = document.createDocumentFragment();
+  for(var i = 0, l = arr.length; i < l; i++) {
+    el.appendChild(arr[i]));
+  }
+  return el;
+}
