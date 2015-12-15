a52e77183e5610ed518cf0a8cd902626e9bd2494
bredele <olivier.wietrich@gmail.com>
2015-12-14 20:31:08 -0700

    remove extra bracket

--- index.js
+++ index.js
@@ -10,7 +10,7 @@
 module.exports = function(arr) {
   var el = document.createDocumentFragment();
   for(var i = 0, l = arr.length; i < l; i++) {
-    el.appendChild(arr[i]));
+    el.appendChild(arr[i]);
   }
   return el;
 }
